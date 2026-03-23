ef5675adae399f5618b95f57b24469adc4011ea1
nosey-dewdrop <damummyphus@gmail.com>
2026-03-23 18:06:05 +0300

    remove search bars, search already in add meal sheet and discover sheet

--- gymgyme/Views/HomeView.swift
+++ gymgyme/Views/HomeView.swift
@@ -78,7 +78,6 @@ struct HomeView: View {
                         .padding(.top, 8)
                     }
                     .background(DoodleTheme.bg.ignoresSafeArea(.all))
-                    .searchable(text: $exerciseSearchText, placement: .navigationBarDrawer(displayMode: .automatic), prompt: "search exercises...")
                     .onSubmit(of: .search) { showDiscoverSheet = true }
                     .navigationBarTitleDisplayMode(.inline)
                     .toolbarBackground(.hidden, for: .navigationBar)
@@ -137,7 +136,6 @@ struct HomeView: View {
                             .padding(.top, 8)
                     }
                     .background(DoodleTheme.bg.ignoresSafeArea(.all))
-                    .searchable(text: $foodSearchText, placement: .navigationBarDrawer(displayMode: .automatic), prompt: "search food...")
                     .navigationBarTitleDisplayMode(.inline)
                     .toolbarBackground(.hidden, for: .navigationBar)
                 }
